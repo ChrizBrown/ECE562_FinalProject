e3f7f16721c10bb633055bbbe843eabae85c31ae
chris <brownca@email.arizona.edu>
2021-04-11 17:37:18 -0500

    Added script for easy file moving

---
+++ mvFiles.sh
@@ -0,0 +1,31 @@
+#SET TO YOUR PERSONAL GEM5 PATH
+GEM5_PATH="../gem5/"
+
+# DO NOT CHANGE ANYTHING AFTER THIS LINE
+PROJECT_PATH=$(pwd)/
+BACKUP_PATH=$(pwd)/Backup/
+
+
+CACHE_FILE=$GEM5_PATH"src/mem/cache/Cache.py"
+RP_FILE=$GEM5_PATH"src/mem/cache/replacement_policies/ReplacementPolicies.py"
+SLRU_HH_FILE=$GEM5_PATH"src/mem/cache/replacement_policies/slru_rp.hh"
+SLRU_CC_FILE=$GEM5_PATH"src/mem/cache/replacement_policies/slru_rp.cc"
+
+
+# file $CACHE_FILE
+# file $RP_FILE
+# file $SLRU_HH_FILE
+# file $SLRU_CC_FILE
+
+# Backup old files
+cp $CACHE_FILE      $BACKUP_PATH
+cp $RP_FILE         $BACKUP_PATH
+cp $SLRU_HH_FILE    $BACKUP_PATH
+cp $SLRU_CC_FILE    $BACKUP_PATH
+
+cp $PROJECT_PATH/Cache.py               $CACHE_FILE
+cp $PROJECT_PATH/ReplacementPolicies.py $RP_FILE
+cp $PROJECT_PATH/slru_rp.hh             $SLRU_HH_FILE
+cp $PROJECT_PATH/slru_rp.cc             $SLRU_CC_FILE
+
+echo "Done!"
